5ade30961360144641cafe41bea0982519e94d66
randseas <alchcraft24@gmail.com>
2024-12-12 18:40:23 +0300

    finih

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,12 @@ export default function Home() {
       animate={{ opacity: 1, x: 0 }}
       transition={{ duration: 0.3 }}
     >
-      <div className="flex max-w-[100vw] overflow-x-hidden justify-center items-center h-[calc(100vh-108px)]">
-        <section className="w-full flex flex-row items-center justify-between px-16">
+      <div className="flex max-w-[100vw] overflow-y-auto mt-[108px] lg:mt-0 overflow-x-hidden justify-center items-center h-full min-h-[calc(100vh-108px)]">
+        <section className="w-full flex mt-20 mb-5 lg:mb-0 lg:mt-0 flex-col lg:flex-row items-center justify-between lg:px-8 px-4 xl:px-16">
           <div className="flex flex-col text-start items-start justify-center">
-            <h1 className="font-bold text-[46px]">$PEKKY to the Moon</h1>
+            <h1 className="font-bold leading-10 md:leading-normal text-[36px] lg:text-[42px] xl:text-[46px]">
+              $PEKKY to the Moon
+            </h1>
             <h2 className="font-medium leading-7 text-xl">
               Basic description whatever you
               <br /> like lorem ipsum dolor sit amet...
@@ -23,8 +25,15 @@ export default function Home() {
               </button>
             </div>
           </div>
-          <div>
-            <img className="rounded-3xl" src="http://placehold.co/512" />
+          <div className="lg:mt-0 mt-7 flex flex-col items-center justify-center">
+            <img
+              className="rounded-3xl lg:flex hidden"
+              src="http://placehold.co/512"
+            />
+            <img
+              className="rounded-3xl lg:hidden flex"
+              src="http://placehold.co/330"
+            />
           </div>
         </section>
       </div>
