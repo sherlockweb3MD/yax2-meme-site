ce36b6898424d65b7fd1e631ebdd0119fea2de26
randseas <alchcraft24@gmail.com>
2024-12-12 18:52:36 +0300

    x

--- app/page.tsx
+++ app/page.tsx
@@ -11,20 +11,6 @@ export default function Home() {
     >
       <div className="flex max-w-[100vw] overflow-y-auto mt-[108px] lg:mt-0 overflow-x-hidden justify-center items-center h-full min-h-[calc(100vh-108px)]">
         <section className="w-full flex mt-20 mb-5 lg:mb-0 lg:mt-0 flex-col lg:flex-row items-center justify-between lg:px-8 px-4 xl:px-16">
-          <div className="flex flex-col text-start items-start justify-center">
-            <h1 className="font-bold leading-10 md:leading-normal text-[36px] lg:text-[42px] xl:text-[46px]">
-              $PEKKY to the Moon
-            </h1>
-            <h2 className="font-medium leading-7 text-xl">
-              Basic description whatever you
-              <br /> like lorem ipsum dolor sit amet...
-            </h2>
-            <div className="mt-4">
-              <button className="font-semibold text-lg px-5 text-white active:scale-[0.98] transition-all ease-linear duration-100 hover:bg-yellow-500/80 active:bg-yellow-500/90 bg-yellow-500/70 py-2.5 rounded-2xl">
-                Buy now
-              </button>
-            </div>
-          </div>
           <div className="lg:mt-0 mt-7 flex flex-col items-center justify-center">
             <img
               draggable="false"
@@ -36,6 +22,20 @@ export default function Home() {
               className="rounded-3xl lg:hidden flex"
               src="http://placehold.co/330"
             />
+          </div>
+          <div className="flex flex-col text-start items-start justify-center">
+            <h1 className="font-bold leading-10 md:leading-normal text-[36px] lg:text-[42px] xl:text-[46px]">
+              $PEKKY to the Moon
+            </h1>
+            <h2 className="font-medium leading-7 text-xl">
+              Basic description whatever you
+              <br /> like lorem ipsum dolor sit amet...
+            </h2>
+            <div className="mt-4">
+              <button className="font-semibold text-lg px-5 text-white active:scale-[0.98] transition-all ease-linear duration-100 hover:bg-yellow-500/80 active:bg-yellow-500/90 bg-yellow-500/70 py-2.5 rounded-2xl">
+                Join now
+              </button>
+            </div>
           </div>
         </section>
       </div>
