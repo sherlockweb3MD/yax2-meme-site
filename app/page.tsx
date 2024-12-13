7efdce00afd7754b398b04457f703d44f5955a38
randseas <alchcraft24@gmail.com>
2024-12-13 16:18:24 +0300

    fca

--- app/page.tsx
+++ app/page.tsx
@@ -23,10 +23,11 @@ export default function Home() {
               $PEKKY to the Moon
             </h1>
             <h2 className="font-medium leading-7 text-xl">
-              Basic description whatever you
-              <br /> like lorem ipsum dolor sit amet...
+              $PEKKY is a funny cartoon duck mascot.
+              <br /> Join the community now and dive into
+              <br /> the meme magic!
             </h2>
-            <div className="mt-4">
+            <div className="mt-3.5">
               <button className="font-semibold text-lg px-5 text-white active:scale-[0.98] transition-all ease-linear duration-100 hover:bg-yellow-500/80 active:bg-yellow-500/90 bg-yellow-500/70 py-2.5 rounded-2xl">
                 Join now
               </button>
